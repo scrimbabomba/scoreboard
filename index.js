d4c3098e7815eb67c5f08a98e4f45a8807398375
scrimbabomba <lukskzk@gmail.com>
2025-03-16 01:29:18 +0100

    Commit initial files

---
+++ index.js
@@ -0,0 +1,15 @@
+let scoreHomeEl = document.getElementById("score-home");
+let scoreGuestEl = document.getElementById("score-guest");
+
+let countHome = 0;
+let countGuest = 0;
+
+function homePlus(points){
+    countHome += points;
+    scoreHomeEl.textContent = countHome;
+}
+
+function guestPlus(points){
+    countGuest += points;
+    scoreGuestEl.textContent = countGuest;
+}
